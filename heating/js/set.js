f2e99df4ac6eb5533f36b71c0eed36d48602bebf
Mimerel <support@mimerel.fr>
2019-12-29 11:09:35 +0100

    [HF][ui] Hour delay not working

--- heating/js/set.js
+++ heating/js/set.js
@@ -1,10 +1,11 @@
 function setTemporary(type) {
-    var valueDay = parseFloat(document.getElementById('day').value);
+    var valueDay = document.getElementById('day').value === "" ? parseFloat(document.getElementById('day').value) : 0;
     var valueHour = parseFloat(document.getElementById('hour').value);
-    if (valueDay !=="" ) {
+
+    if (!isNaN(valueDay)) {
         valueHour = valueHour + valueDay * 24;
     }
     ipPort = document.getElementById('ipPort').value;
-    url = 'http://' + ipPort + '/heating/temporary/' + type +'/' +valueHour;
+    url = 'http://' + ipPort + '/heating/temporary/' + type + '/' + valueHour;
     window.location.assign( url);
 }
