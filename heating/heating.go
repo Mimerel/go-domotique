841dfa6312dc66f128f699765e4ab6e84e4ec78e
Steven LEGUIN <s.leguin@mimerel.fr>
2022-09-19 11:29:34 +0200

    [UP] Split Templates

--- heating/heating.go
+++ heating/heating.go
@@ -11,13 +11,21 @@ import (
 )
 
 func StatusPage(w http.ResponseWriter, r *http.Request, config *models.Configuration) {
+	files := []string{
+		"./heating/templates/Roller.html",
+		"./heating/templates/Switch.html",
+		"./heating/templates/Radiator.html",
+		"./heating/templates/Shelly4PM.html",
+		"./heating/templates/ShellyHT.html",
+		"./heating/templates/status.html",
+	}
 	t := template.New("status.html")
 	t = template.Must(t.Funcs(TemplateGlobal.GetUIDict()).Funcs(TemplateGlobal.GetUIFunctions()).ParseGlob("./heating/templates/*.html"))
-	t, err := t.ParseFiles("./heating/templates/status.html")
+	t, err := t.ParseFiles(files...)
 	if err != nil {
-		logger.Error(config, false,"StatusPage", "Error Parsing template%+v", err)
+		logger.Error(config, false, "StatusPage", "Error Parsing template%+v", err)
 	}
-	logger.DebugPlus(config, false,"StatusPage", "Getting Heating Status")
+	logger.DebugPlus(config, false, "StatusPage", "Getting Heating Status")
 
 	data, err := HeatingStatus(config)
 	if err != nil {
@@ -25,20 +33,20 @@ func StatusPage(w http.ResponseWriter, r *http.Request, config *models.Configura
 	}
 	data.Devices = config.Devices.DevicesToggle
 	config.Channels.MqttCall <- true
-	deviceData := <- config.Channels.MqttReceive
+	deviceData := <-config.Channels.MqttReceive
 	data.DevicesNew = deviceData.ToArray()
 	data.Totals.Watts = deviceData.TotalWatts
 
 	err = t.Execute(w, data)
 	if err != nil {
-		logger.Error(config, false,"StatusPage", "Error Execution %+v", err)
+		logger.Error(config, false, "StatusPage", "Error Execution %+v", err)
 	}
 }
 
 func Update(w http.ResponseWriter, r *http.Request, config *models.Configuration) {
 	config.Logger.Info("update Asked...")
 	config.Channels.MqttCall <- true
-	deviceData := <- config.Channels.MqttReceive
+	deviceData := <-config.Channels.MqttReceive
 	data := deviceData.ToArray()
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(data)
@@ -53,7 +61,7 @@ func Update(w http.ResponseWriter, r *http.Request, config *models.Configuration
 func RunAction(config *models.Configuration, idString string, action string, payload string) {
 	id, err := strconv.Atoi(idString)
 	if err != nil {
-		logger.Error(config, false, "runAction", "Error Converting Id to int64 <%v> : %v",idString, err)
+		logger.Error(config, false, "runAction", "Error Converting Id to int64 <%v> : %v", idString, err)
 		return
 	}
 	actionParams := models.MqttSendMessage{
@@ -64,5 +72,3 @@ func RunAction(config *models.Configuration, idString string, action string, pay
 	config.Logger.Warn("request ; %v", actionParams)
 	config.Channels.MqttSend <- actionParams
 }
-
-
